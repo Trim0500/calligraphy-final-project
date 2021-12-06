48ff7f9f20484731314737248f07e32acedf1ac3
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-12-06 09:51:38 -0500

    Established a baseline nav bar

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="navHeader">
+                <ul className="navBar">
+                    <li className="navItem">
+                        <a className="navLink" href="/">Home</a>
+                    </li>
+                    <li className="navItem">
+                        <a className="navLink" href="/">About the Business</a>
+                    </li>
+                    <li className="navItem">
+                        <a className="navLink" href="/">Portfolio</a>
+                    </li>
+                    <li className="navItem">
+                        <a className="navLink" href="/">Services Information</a>
+                    </li>
+                    <li className="navItem">
+                        <a className="navLink" href="/">Request a Service</a>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+class Page extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <Header />
+                {/* <Content />
+                <Footer /> */}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render (
+    <Page />,
+    document.getElementById('root')
+);
