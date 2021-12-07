2dd2dc01810ebec18397d6c2bc569c6d9ebaf0e4
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-12-07 09:38:34 -0500

    Updated the footer component

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
 
 function Footer(){
     return(
-        <div>Allo</div>
+        <Navbar bg="light" expand="lg" fixed="bottom">
+            <Container>
+                <p>Serene Florish &#169; 2022</p>
+            </Container>
+        </Navbar>
     );
 }
 
