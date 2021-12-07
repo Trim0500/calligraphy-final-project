3206894f96132aa67601f73a093c2887045f325b
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-12-07 09:28:53 -0500

    Added bootstrap styling to the project

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,26 +1,29 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Container from 'react-bootstrap/Container'
 
 function Header() {
     return (
-        <div className="navHeader">
-            <ul className="navBar">
-                <li className="navItem">
-                    <a className="navLink" href="/">Home</a>
-                </li>
-                <li className="navItem">
-                    <a className="navLink" href="/">About the Business</a>
-                </li>
-                <li className="navItem">
-                    <a className="navLink" href="/">Portfolio</a>
-                </li>
-                <li className="navItem">
-                    <a className="navLink" href="/">Services Information</a>
-                </li>
-                <li className="navItem">
-                    <a className="navLink" href="/">Request a Service</a>
-                </li>
-            </ul>
-        </div>
+        <Navbar bg="light" expand="lg" fixed="top" position="sticky">
+            <Container>
+                <Navbar.Brand href="/">Serene Florish</Navbar.Brand>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/">About the Business</Nav.Link>
+                        <Nav.Link href="/">Portfolio</Nav.Link>
+                        <NavDropdown title="Services" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/">Gallery</NavDropdown.Item>
+                            <NavDropdown.Item href="/">Pricing</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="/">Request a Service</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
