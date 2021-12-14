e78bc5b42af41ec3e03c2a9f16bd2e8f2f32cfc2
Jordan Albayrak <1830867@champlaincollege.qc.ca>
2021-12-14 11:27:11 -0500

    Implemented the address and FName and LName to the form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,13 +2,29 @@ import React, {useState} from "react";
 
 
 function Form(){
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [address, setAddress] = useState({street:"", postal:"", city:"", country:""});
+
+    const[street, setStreet] = useState('');
+    const[postal, setPotal] = useState('');
+    const[city, setCity] = useState('');
+    const[country, setCountry] = useState('');
+
 
     const [service, setService] = useState('');
     const [comments, setComments] = useState('');
 
+
     const [submit, setSubmit] = useState(false);
     const [errorNullInputs, setErrorNullInputs] = useState(false);
 
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value);
+    }
+    const handleLastName = (e) => {
+        setLastName(e.target.value);
+    }
     const handleService = (e) => {
         setService(e.target.value);
     }
@@ -16,14 +32,18 @@ function Form(){
         setComments(e.target.value);
     }
 
+
     // const changeHandler = (e) => {
     //     this.setState({[e.target.name]: e.target.value})
     // }
     const handleSubmission = () => {
         // assign api to a value
-        let api = 'https://localhost:44302/api/form';
+        let api = 'https://localhost:44386/api/form';
         // create a new object
         let newServiceRequest = {
+            FirstName: firstName,
+            LastName: lastName,
+            Address: address,
             ServiceType: service,
             Comments: comments
         }
@@ -49,9 +69,9 @@ function Form(){
         setErrorNullInputs(false);
         setSubmit(false);
 
-        if (comments === '' | comments === '\n') {
+        if (comments === '' || service === '') {
             setErrorNullInputs(true);
-            alert(`Failed, Comment is required`);
+            alert(`Failed, All Info is required`);
             event.preventDefault();
             console.log(service, comments);
         }
@@ -60,7 +80,7 @@ function Form(){
             handleSubmission();
             alert(`Success, service request sent!`)
             event.preventDefault();
-            console.log(service, comments);
+            console.log(firstName, lastName, street, postal, city, country ,service, comments);
         }
     }
 
@@ -104,28 +124,46 @@ function Form(){
                         </div>
                         <div className="card-body">
                             <form onSubmit={handleSubmit}>
-                            <div>
-                                <div className="form-group">
-                                    <label htmlFor="service">Service Type</label>
-                                    <select className="form-control" name="service" value={service} onChange={handleService}>
-                                        <option value="calligraphy" name="calligraphy-select">Calligraphy</option>
-                                        <option value="engraving" name="engraving-select">Engraving</option>
-                                        <option value="event" name="event-select">Event</option>
-                                    </select>
+                                <div>
+                                    <div className="form-group">
+                                        <label htmlFor="firstName">First Name</label>
+                                        <textarea className="form-control" name="firstName" value={firstName} onChange={handleFirstName}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="lastName">Last Name</label>
+                                        <textarea className="form-control" name="firstName" value={lastName} onChange={handleLastName}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="Street">Street</label>
+                                        <textarea className="form-control" name="street" onChange={(e) => setAddress({...address, street: e.target.value})} value={address.street} />
+                                        <label htmlFor="Postal">Postal Code</label>
+                                        <textarea className="form-control" name="postal" onChange={(e) => setAddress({...address, postal: e.target.value})} value={address.postal} />
+                                        <label htmlFor="City">City</label>
+                                        <textarea className="form-control" name="city" onChange={(e) => setAddress({...address, city: e.target.value})} value={address.city}/>
+                                        <label htmlFor="Country">Country</label>
+                                        <textarea className="form-control" name="country" onChange={(e) => setAddress({...address, country: e.target.value})} value={address.country} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="service">Service Type</label>
+                                        <select className="form-control" name="service" value={service} onChange={handleService}>
+                                            <option value="calligraphy" name="calligraphy-select">Calligraphy</option>
+                                            <option value="engraving" name="engraving-select">Engraving</option>
+                                            <option value="event" name="event-select">Event</option>
+                                        </select>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="comments">Comments</label>
+                                        <textarea className="form-control" name="comments" value={comments} onChange={handleComments}/>
+                                    </div>
+                                    <button type="submit" className="btn btn-primary" name="submit-btn">Submit</button>
                                 </div>
-                                <div className="form-group">
-                                    <label htmlFor="comments">Comments</label>
-                                    <textarea className="form-control" name="comments" value={comments} onChange={handleComments}/>
-                                </div>
-                                <button type="submit" className="btn btn-primary" name="submit-btn">Submit</button>
-                            </div>
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-     );
-    }
+    );
+}
 
 export default Form;
