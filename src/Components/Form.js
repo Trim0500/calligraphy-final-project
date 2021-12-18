ca9dd58f1be172a00557b1a0f96a11d742b9a870
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-12-18 17:56:42 -0500

    Fixed the empty fields bug and tidied up the code

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,20 +2,19 @@ import React, {useState} from "react";
 
 
 function Form(){
+    const [firstName, setFirstname] = useState('');
 
-    const [customer, setCustomer] = useState({firstName:"", lastName:"", email: "", address:{street:"", postal:"", city:"", country:""}});
+    const [lastName, setLastName] = useState('');
 
-    const [firstName] = useState('');
-    const [lastName] = useState('');
-    const [email] = useState('');
+    const [email, setEmail] = useState('');
 
-    const[street] = useState('');
-    const[postal] = useState('');
-    const[city] = useState('');
-    const[country] = useState('');
-
+    const[street, setStreet] = useState('');
+    const[postal, setPostal] = useState('');
+    const[city, setCity] = useState('');
+    const[country, setCountry] = useState('');
 
     const [service, setService] = useState('');
+
     const [comments, setComments] = useState('');
 
     const [selectedFile, setSelectedFile] = useState(null);
@@ -23,9 +22,35 @@ function Form(){
     const [submit, setSubmit] = useState(false);
     const [errorNullInputs, setErrorNullInputs] = useState(false);
 
+    const handleFirstName = (e) => {
+        setFirstname(e.target.value);
+    }
+
+    const handleLastName = (e) => {
+        setLastName(e.target.value);
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handleStreet = (e) => {
+        setStreet(e.target.value);
+    }
+    const handleCity = (e) => {
+        setCity(e.target.value);
+    }
+    const handleCountry = (e) => {
+        setCountry(e.target.value);
+    }
+    const handlePostal = (e) => {
+        setPostal(e.target.value);
+    }
+
     const handleService = (e) => {
         setService(e.target.value);
     }
+
     const handleComments = (e) => {
         setComments(e.target.value);
     }
@@ -34,29 +59,25 @@ function Form(){
         setSelectedFile(e.target.files[0]);
     }
 
-    // const changeHandler = (e) => {
-    //     this.setState({[e.target.name]: e.target.value})
-    // }
     const handleFormSubmission = () => {
-        // assign api to a value
         let api = 'https://localhost:5001/api/form';
-        // create a new object
+        
         let newServiceRequest = {
             Customer: {
-                FirstName: customer.firstName,
-                LastName: customer.lastName,
-                Email: customer.email,
+                FirstName: firstName,
+                LastName: lastName,
+                Email: email,
                 Address: {
-                    Street: customer.address.street,
-                    Postal: customer.address.postal,
-                    City: customer.address.city,
-                    Country: customer.address.country
+                    Street: street,
+                    Postal: postal,
+                    City: city,
+                    Country: country
                 }
             },
             ServiceType: service,
             Comments: comments
         }
-        // fetch the api
+        
         fetch(api, {
             method: 'POST',
             headers: {
@@ -74,12 +95,12 @@ function Form(){
     const handleEmailRequest = () => {
         let api = 'https://localhost:5001/Mailer/Send';
 
-        let email = customer.email;
+        let emailTo = email;
         let subject = "Request for " + service;
         let today = new Date();
         let date = today.getDate() + "-" + parseInt(today.getMonth() + 1) + "-" + today.getFullYear();
         let body = '<h1>Greetings from Serene Flourish!</h1>';
-        body += '<h3>Hello ' + customer.firstName + '!</h3>';
+        body += '<h3>Hello ' + firstName + '!</h3>';
         body += '<p>We\'ve received your order and will contact you as soon as your package is shipped. You can find your purchase information below.</p>';
         body += '<h3>Order Summary</h3>';
         body += '<p>' + date + '</p>';
@@ -88,17 +109,18 @@ function Form(){
         body += '<h3>Customization Comments</h3>';
         body += '<p>' + comments + '</p>';
         body += '<h3>Your Contact Information</h3>';
-        body += '<p>' + customer.firstName + ' ' + customer.lastName + '<p>';
-        body += '<p>Address: ' + customer.address.street + ' ' + customer.address.city + ' ' + customer.address.country + ' ' + customer.address.postal + '</p>';
-        body += '<p>Email: ' + customer.email + '</p>';
+        body += '<p>' + firstName + ' ' + lastName + '<p>';
+        body += '<p>Address: ' + street + ' ' + city + ' ' + country + ' ' + postal + '</p>';
+        body += '<p>Email: ' + email + '</p>';
         body += '<h3>This is a auto-generated Quote and may be subject to change. If there are any changes we encounter, we will contact you again to receive your approval.</h3>'
         let file = selectedFile;
 
         var dataPayload = new FormData();
-        dataPayload.append("email", email);
+        dataPayload.append("email", emailTo);
         dataPayload.append("subject", subject);
         dataPayload.append("body", body);
         dataPayload.append("attachtments", file, file.name);
+
         console.log("Email: " + dataPayload.get("email"));
         console.log("Subject: " + dataPayload.get("subject"));
         console.log("Body: " + dataPayload.get("body"));
@@ -116,28 +138,45 @@ function Form(){
     }
 
     const handleSubmit = (event) => {
-        // alert(`Success! \nService Type: ${service}, \nComment: ${comments}`)
-        // event.preventDefault();
-        // console.log(service, comments);
-
         setErrorNullInputs(false);
         setSubmit(false);
 
         if (firstName === '' || lastName === '' || street ==='' || postal === '' || city === '' || country === ''|| comments === '' || service === '') {
             setErrorNullInputs(true);
+
             alert(`Failed, All Info is required`);
+
             event.preventDefault();
-            console.log(service, comments);
+
+            console.log(firstName, lastName, email, street, postal, city, country ,service, comments);
         }
         else {
             setSubmit(true);
+
             handleFormSubmission();
+
             alert(`Success, service request sent!`)
+
             event.preventDefault();
+
             handleEmailRequest();
+
             alert("Thank you for your request, an email has been sent your way!")
+
             event.preventDefault();
+            
             console.log(firstName, lastName, email, street, postal, city, country ,service, comments);
+
+            firstName.useState('');
+            lastName.useState('');
+            email.useState('');
+            street.useState('');
+            city.useState('');
+            country.useState('');
+            postal.useState('');
+            service.useState('');
+            comments.useState('');
+            selectedFile.useState(null);
         }
     }
 
@@ -184,25 +223,25 @@ function Form(){
                                 <div>
                                     <div className="form-group">
                                         <label htmlFor="firstName">First Name</label>
-                                        <input className="form-control" name="firstName"  onChange={(e) => setCustomer({...customer, firstName: e.target.value})} value={customer.firstName}/>
+                                        <input className="form-control" name="firstName"  onChange={handleFirstName} value={firstName}/>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="lastName">Last Name</label>
-                                        <input className="form-control" name="lastName"onChange={(e) => setCustomer({...customer, lastName: e.target.value})} value={customer.lastName}/>
+                                        <input className="form-control" name="lastName"onChange={handleLastName} value={lastName}/>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="email">Email</label>
-                                        <input className="form-control" name="email" onChange={(e) => setCustomer({...customer, email: e.target.value})} value={customer.email}/>
+                                        <input className="form-control" name="email" onChange={handleEmail} value={email}/>
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="Street">Street</label>
-                                        <input className="form-control" name="street" onChange={(e) => setCustomer({ ...customer, address: { ...customer.address, street: e.target.value } })} value={customer.address.street} />
+                                        <input className="form-control" name="street" onChange={handleStreet} value={street} />
                                         <label htmlFor="Postal">Postal Code</label>
-                                        <input className="form-control" name="postal" onChange={(e) => setCustomer({ ...customer, address: { ...customer.address, postal: e.target.value } })} value={customer.address.postal} />
+                                        <input className="form-control" name="postal" onChange={handlePostal} value={postal} />
                                         <label htmlFor="City">City</label>
-                                        <input className="form-control" name="city" onChange={(e) => setCustomer({ ...customer, address: { ...customer.address, city: e.target.value } })} value={customer.address.city} />
+                                        <input className="form-control" name="city" onChange={handleCity} value={city} />
                                         <label htmlFor="Country">Country</label>
-                                        <input className="form-control" name="country" onChange={(e) => setCustomer({ ...customer, address: { ...customer.address, country: e.target.value } })} value={customer.address.country} />
+                                        <input className="form-control" name="country" onChange={handleCountry} value={country} />
                                     </div>
                                     <div className="form-group">
                                         <label htmlFor="service">Service Type</label>
