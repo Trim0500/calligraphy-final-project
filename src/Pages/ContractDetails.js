4cf09c89a228187e267655ccee22fa7e0f48b03b
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2022-01-13 10:32:59 -0500

    Finished the update page

--- src/Pages/ContractDetails.js
+++ src/Pages/ContractDetails.js
@@ -7,6 +7,7 @@ export default function ContractDetails() {
 
     const [FinalCost, setFinalCost] = useState('');
     const [DownPayment, setDownPayment] = useState('');
+    const [OriginalDate, setOriginalDate] = useState('');
     const [StartDate, setStartDate] = useState('');
     const [DueDate, setDueDate] = useState('');
     const [HasSignature, setHasSignature] = useState(false);
@@ -29,6 +30,7 @@ export default function ContractDetails() {
         var StartDate = new Date(fetchedStartDate);
         var IsoStartDate = StartDate.toISOString().substring(0, 10);
         setStartDate(IsoStartDate);
+        setOriginalDate(IsoStartDate);
 
         var fetchedEndDate = json.EndDate;
         var EndDate = new Date(fetchedEndDate);
@@ -36,12 +38,88 @@ export default function ContractDetails() {
         setDueDate(IsoEndDate);
     }
 
-    const HandleChecked = () => {
+    const HandleChecked = (e) => {
+        const target = e.target;
+        const value = target.checked;
+        const name = target.name;
 
+        switch(name) {
+            case 'HasSignature':
+                setHasSignature(value);
+                break;
+            case 'IsFinished':
+                setIsFinished(value);
+                break;
+            default:
+                break;
+        }
     }
 
-    const HandleChange = () => {
+    const HandleChange = (e) => {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
 
+        switch(name) {
+            case 'FinalCost':
+                setFinalCost(value);
+                break;
+            case 'DownPayment':
+                setDownPayment(value);
+                break;
+            case 'DateCommissioned':
+                setStartDate(value);
+                break;
+            case 'EndDate':
+                setDueDate(value);
+                break;
+            default:
+                break;
+        }
+    }
+
+    const HandleSubmit = (event) => {
+        if(FinalCost === '' ||
+           DownPayment === '' ||
+           StartDate === '' ||
+           DueDate === '') {
+            alert(`Failed, All Info is required`);
+
+            event.preventDefault();
+           }
+        else if(StartDate < OriginalDate) {
+            alert(`Failed, you can't set the start date to an earlier date`);
+
+            event.preventDefault();
+        }
+        else {
+            let UpdateContractRequest = {
+                ContractId: data.id,
+                FinalCost: FinalCost,
+                DownPayment: DownPayment,
+                DateCommissioned: StartDate,
+                EndDate: DueDate,
+                HasSignature: HasSignature,
+                IsFinished: IsFinished
+            }
+
+            let api = 'https://localhost:5001/api/contract/update'
+
+            fetch(api, {
+                method: 'PUT',
+                body: JSON.stringify(UpdateContractRequest),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err));
+
+            alert('Success! The contract has been updated!');
+
+            event.preventDefault();
+        }
     }
 
     useEffect(() => {
@@ -50,7 +128,7 @@ export default function ContractDetails() {
 
             await fetch(api, {
                 headers: {
-                    'COntent-Type': 'application/json',
+                    'Content-Type': 'application/json',
                     'Accept': 'application/json'
                 }
             })
@@ -63,6 +141,7 @@ export default function ContractDetails() {
             .catch((err) => console.log(err));
         }
         getContract();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [data])
 
     return(
@@ -72,13 +151,13 @@ export default function ContractDetails() {
                 <div className="col-md-6 mx-auto">
                     <div className="card">
                         <div className="card-body">
-                            <form>
+                            <form onSubmit={HandleSubmit}>
                                 <div className="form-group">
                                     <label htmlFor="FinalCost">Final Cost</label>
-                                    <input className="form-control" name="FinalCost" value={FinalCost} onChange={HandleChange}/>
+                                    <input type="number" className="form-control" name="FinalCost" value={FinalCost} onChange={HandleChange}/>
 
                                     <label htmlFor="DownPayment">Down Payment</label>
-                                    <input className="form-control" name="DownPayment" value={DownPayment} onChange={HandleChange}/>
+                                    <input type="number" className="form-control" name="DownPayment" value={DownPayment} onChange={HandleChange}/>
 
                                     <label htmlFor="DateCommissioned">Date Commissioned</label>
                                     <input type="date" className="form-control" name="DateCommissioned" value={StartDate} onChange={HandleChange}/>
@@ -87,10 +166,10 @@ export default function ContractDetails() {
                                     <input type="date" className="form-control" name="EndDate" value={DueDate} onChange={HandleChange}/>
 
                                     <label htmlFor="HasSignature">Has Signature?</label><br/>
-                                    <input type="checkbox" name="HasSignature" checked={HasSignature} onChange={HandleChecked}/><br/>
+                                    <input type="checkbox" disabled={HasSignature} name="HasSignature" checked={HasSignature} onChange={HandleChecked}/><br/>
 
                                     <label htmlFor="IsFinished">Is Finished?</label><br/>
-                                    <input type="checkbox" name="IsFinished" checked={IsFinished} onChange={HandleChecked}/><br/>
+                                    <input type="checkbox" disabled={IsFinished} name="IsFinished" checked={IsFinished} onChange={HandleChecked}/><br/>
 
                                     <button type="submit" className="btn btn-primary" name="SubmitBtn">Update Contract</button>
                                 </div>
