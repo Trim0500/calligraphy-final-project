5b97b3bad7c66cd99a98004d61884b4b22d761fe
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2022-01-26 17:05:29 -0500

    Added names to earnings elements

--- src/Pages/EarningsPage.js
+++ src/Pages/EarningsPage.js
@@ -121,33 +121,33 @@ export default function EarningsPage() {
                     <Card.Title>Earnings</Card.Title>
                 </Card.Header>
                 <Card.Body>
-                    <h1>Monthly Earnings Page</h1>
+                    <h1 name="EarningsHeader">Monthly Earnings Page</h1>
                     <br/>
                     <form onSubmit={HandleSubmit}>
-                        <select className='form-control' onChange={HandleSelectedMonth}>
-                            <option>Janurary</option>
-                            <option>Feburary</option>
-                            <option>March</option>
-                            <option>April</option>
-                            <option>May</option>
-                            <option>June</option>
-                            <option>July</option>
-                            <option>August</option>
-                            <option>September</option>
-                            <option>October</option>
-                            <option>November</option>
-                            <option>December</option>
+                        <select name="MonthSelect" className='form-control' onChange={HandleSelectedMonth}>
+                            <option name="JanOption">Janurary</option>
+                            <option name="FebOption">Feburary</option>
+                            <option name="MarOption">March</option>
+                            <option name="AprOption">April</option>
+                            <option name="MayOption">May</option>
+                            <option name="JunOption">June</option>
+                            <option name="JulOption">July</option>
+                            <option name="AugOption">August</option>
+                            <option name="SepOption">September</option>
+                            <option name="OctOption">October</option>
+                            <option name="NovOption">November</option>
+                            <option name="DecOption">December</option>
                         </select>
-                        <input className='form-control' type="number" defaultValue="2021" max="2022" min="2010" onChange={HandleYearChange} />
-                        <button className='btn btn-primary' type='submit'>Find Contracts</button>
+                        <input name="YearInput" className='form-control' type="number" defaultValue="2021" max="2022" min="2010" onChange={HandleYearChange} />
+                        <button name="SubmitBtn" className='btn btn-primary' type='submit'>Find Contracts</button>
                     </form>
                     <br/>
-                    <ul className='nav nav-tabs' style={{display: 'flex', justifyContent: 'space-evenly' }}>
-                        <li><h3>Total charged this month: ${TotalCharged}</h3></li>
-                        <li><h3>Total contracts: {TotalContracts}</h3></li>
+                    <ul name="StatsList" className='nav nav-tabs' style={{display: 'flex', justifyContent: 'space-evenly' }}>
+                        <li name="ChargedStat"><h3 name="ChargedHeader">Total charged this month: ${TotalCharged}</h3></li>
+                        <li name="TotalStat"><h3 name="TotalHeader">Total contracts: {TotalContracts}</h3></li>
                     </ul>
-                    <div style={{margin: 'auto', width: '50%', height: '500px'}}>
-                        <Chart data={dataChart} axes={axes} />
+                    <div name="ChartDiv" style={{margin: 'auto', width: '50%', height: '500px'}}>
+                        <Chart name="Chart" data={dataChart} axes={axes} />
                     </div>
                 </Card.Body>
             </Card>
