de7e9db3b913f027ef8ef48a2e4bdd902e4e4719
Jordan Albayrak <jordan008@Live.ca>
2022-02-07 20:56:44 -0500

    ServiceType Filter is functional now, next step is Date Filtering.

--- src/Pages/FormOperation.js
+++ src/Pages/FormOperation.js
@@ -5,12 +5,21 @@ import {Card} from "react-bootstrap";
 import axios from "axios";
 // eslint-disable-next-line
 import Interceptor from "../Components/interceptor";
+import Select from 'react-select'
 
 function FormOperation(){
 
     const [form, setForm] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [pageSize, setPageSize] = useState(10);
+    const [serviceTypeFilter, setServiceTypeFilter] = useState('All');
+    const [createdDateFilter, setCreatedDateFilter] = useState('');
+
+    const options = [
+        { value: 'all', label: 'All' },
+        { value: 'calligraphy', label: 'Calligraphy' },
+        { value: 'engraving', label: 'Engraving' },
+    ]
 
     const GetForms = () => {
 
@@ -127,8 +136,16 @@ function FormOperation(){
             });
     };
 
+    function ServiceTypeChange(e) {
+        setServiceTypeFilter(e.label);
+    }
+    function handleDateFilter(e) {
+        setCreatedDateFilter(e.target.value);
+    }
+
     useEffect(() => {
         GetForms();
+
     }, []);
 
     return (
@@ -138,6 +155,22 @@ function FormOperation(){
                     <Card.Title>Forms</Card.Title>
                 </Card.Header>
                 <Card.Body>
+                    <form method="get" className={''}>
+                        <div className={'d-inline-flex w-75'}>
+                            <Select options={options} onChange={ServiceTypeChange} defaultValue={'All'} className={'w-25'} placeholder={'Service Type'}/>
+                            <input type="date" name="DateCommissioned" className={'w-25'} value={createdDateFilter} onChange={handleDateFilter}/>
+                        <label htmlFor="header-search">
+                            <span className="visually-hidden">Search Specific Form</span>
+                        </label>
+                        <input
+                            type="text"
+                            id="header-search"
+                            placeholder="Search Specific Form"
+                            name="s"
+                        />
+                        <button type="submit">Search</button>
+                        </div>
+                    </form>
                     <table className="table table-striped">
                         <thead>
                         <tr>
@@ -147,14 +180,21 @@ function FormOperation(){
                         </tr>
                         </thead>
                         <tbody>
-                        {form.map(form => (
+                        {form.map(form => (serviceTypeFilter !== 'All' || createdDateFilter !== '' ? (form.ServiceType === serviceTypeFilter || form.CreatedDate === "2022-01-27T15:54:39.4146222" ?
                             <tr key={form.FormId}>
                                 <td>{form.ServiceType}</td>
                                 <td>{form.Comments}</td>
                                 <td>{form.CreatedDate}</td>
                                 <td><button name="btnQuote" className={"btn-primary"}><a href={"/admin/dashboard/quote/" + form.FormId } className={"text-white text-decoration-none form-control-sm"}>See Quote</a> </button></td>
                             </tr>
-                        ))}
+                         : null) :
+                            <tr key={form.FormId}>
+                            <td>{form.ServiceType}</td>
+                            <td>{form.Comments}</td>
+                            <td>{form.CreatedDate}</td>
+                            <td><button name="btnQuote" className={"btn-primary"}><a href={"/admin/dashboard/quote/" + form.FormId } className={"text-white text-decoration-none form-control-sm"}>See Quote</a> </button></td>
+                        </tr>))}
+
                         </tbody>
                     </table>
                     <button onClick={nextPage} name="btnNext">Next Page</button>
