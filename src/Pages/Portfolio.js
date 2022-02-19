48c3da3cb739c23e30e1a4a2a96175106b1fc029
Jeremy Forest <53579676+Jaylapuck@users.noreply.github.com>
2022-02-19 14:02:24 -0500

    update for now

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,75 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import {Col, Container, Image, Row} from 'react-bootstrap';
 import no_image from "../resources/img/no_image.png";
 import {AnimatePresence, motion} from 'framer-motion/dist/framer-motion'
+import {Nav} from "react-bootstrap";
+import ImagePortfolio from "../Components/ImagePortfolio.js";
+import {NavLink} from "reactstrap";
+
 
 export  default function Portfolio() {
 
-    // this is incredibly lazy but I might change it later
-    const [imageData1, setImageData1] = React.useState([]);
-    const [imageData2, setImageData2] = React.useState([]);
-    const [imageData3, setImageData3] = React.useState([]);
-    const [imageData4, setImageData4] = React.useState([]);
-    const [imageData5, setImageData5] = React.useState([]);
-    const [imageData6, setImageData6] = React.useState([]);
+    const [imageData1, setImageData1] = useState([]);
+    const [imageData2, setImageData2] = useState([]);
+    const [imageData3, setImageData3] = useState([]);
+    const [imageData4, setImageData4] = useState([]);
+    const [imageData5, setImageData5] = useState([]);
+    const [imageData6, setImageData6] = useState([]);
 
-    const containerVariants = {
-        hidden: {
-            opacity: 0,
-            x: '-100vw'
-        },
-        visible: {
-            opacity: 1,
-            x: 0,
-            transition: {
-                type: 'spring',
-                delay: 0.5,
-                stiffness: 50,
-                mass: 0.5,
-                duration: 0.5
-            }
-        },
-        exit: {
-            x: '-100vw',
-            transition: {
-                type: 'spring',
-                stiffness: 50,
-                mass: 0.5,
-                duration: 0.5
-            }
-        }
-    };
-
-    const imageVariants = {
-        hidden: {
-            opacity: 0,
-            x: '-100vw'
-        },
-        visible: {
-            opacity: 1,
-            x: 0,
-            transition: {
-                type: 'spring',
-                delay: 0.5,
-                stiffness: 50,
-                mass: 0.5,
-                duration: 0.5
-            }
-        },
-        exit: {
-            x: '-100vw',
-            transition: {
-                type: 'spring',
-                stiffness: 50,
-                mass: 0.5,
-                duration: 0.5
-            }
-        }
-    };
-
-
-    React.useEffect(() => {
+    useEffect(() => {
         fetch('https://localhost:5001/api/image', {
             headers: {
                 'Accept': 'application/json',
@@ -79,30 +27,25 @@ export  default function Portfolio() {
         })
             .then(response => response.json())
             .then(data => {
-                for (let i = 0; i < 6; i++) {
-                    //check if imageId is 1
+                console.log(data);
+                for (let i = 0; i < data.length ; i++) {
                     if (data[i].ImageId === 1) {
-                        setImageData1(data[0]);
+                        setImageData1(data[i]);
                     }
-                    //check if imageId is 2
                     if (data[i].ImageId === 2) {
-                        setImageData2(data[1]);
+                        setImageData2(data[i]);
                     }
-                    //check if imageId is 3
                     if (data[i].ImageId === 3) {
-                        setImageData3(data[2]);
+                        setImageData3(data[i]);
                     }
-                    //check if imageId is 4
                     if (data[i].ImageId === 4) {
-                        setImageData4(data[3]);
+                        setImageData4(data[i]);
                     }
-                    //check if imageId is 5
                     if (data[i].ImageId === 5) {
-                        setImageData5(data[4]);
+                        setImageData5(data[i]);
                     }
-                    //check if imageId is 6
                     if (data[i].ImageId === 6) {
-                        setImageData6(data[5]);
+                        setImageData6(data[i]);
                     }
                 }
             })
@@ -111,181 +54,32 @@ export  default function Portfolio() {
 
     return (
 
-        //seperate images by different types, signature, events and engraving
         <AnimatePresence>
-            <motion.div
-                initial="hidden"
-                animate="visible"
-                exit="exit"
-                variants={containerVariants}
+            <div
                 className="container-fluid">
                 <Container className="mt-5">
                     <Row>
                         <Col>
                             <h1 className="text-center">Signature</h1>
                             <h3 className="text-center ">$100 - $200</h3>
-                            <motion.div
-                                initial="hidden"
-                                animate="visible"
-                                exit="exit"
-                                variants={imageVariants}
-                                className="image-container card m-5">
-                                <Image className="card-img" src={imageData1.ImageData} fluid/>
-                                <div className="card-body">
-                                    <h5 className="card-title fw-bold">
-                                        {
-                                            imageData1.Title === "" ? "Untitled" : imageData1.Title
-                                        }
-                                    </h5>
-                                    <p className="card-text">
-                                        {
-                                            imageData1.Description === "" ? "No description" : imageData1.Description
-                                        }
-                                    </p>
-                                </div>
-                            </motion.div>
+                            <ImagePortfolio image={imageData1.ImageData} title={imageData1.Title} description={imageData1.Description}/>
+                            <ImagePortfolio image={imageData2.ImageData} title={imageData2.Title} description={imageData2.Description}/>
                         </Col>
                         <Col>
                             <h1 className="text-center">Events</h1>
-                            <h3 className="text-center">$100 - $200</h3>
-                            <motion.div
-                                initial="hidden"
-                                animate="visible"
-                                exit="exit"
-                                variants={imageVariants}
-                                className="image-container card m-5">
-                                <Image className="card-img" src={
-                                    imageData2 ?
-                                        no_image :
-                                        imageData2.ImageData
-                                } fluid/>
-                                <div className="card-body">
-                                    <h5 className="card-title fw-bold">
-                                        {
-                                            imageData2 ? "Untitled" : imageData2.Title
-                                        }
-                                    </h5>
-                                    <p className="card-text">
-                                        {
-                                            imageData2 ? "No description" : imageData2.Description
-                                        }
-                                    </p>
-                                </div>
-                            </motion.div>
+                            <h3 className="text-center ">$200 - $300</h3>
+                            <ImagePortfolio image={imageData3.ImageData} title={imageData3.Title} description={imageData3.Description}/>
+                            <ImagePortfolio image={imageData4.ImageData} title={imageData4.Title} description={imageData4.Description}/>
                         </Col>
                         <Col>
                             <h1 className="text-center">Engravings</h1>
-                            <h3 className="text-center">$100 - $200</h3>
-                            <motion.div
-                                initial="hidden"
-                                animate="visible"
-                                exit="exit"
-                                variants={imageVariants}
-                                className="image-container card m-5">
-                                <Image className="card-img" src={
-                                    imageData3 ?
-                                        no_image :
-                                        imageData3.ImageData
-
-                                } fluid/>
-                                <div className="card-body">
-                                    <h5 className="card-title fw-bold">
-                                        {
-                                            imageData3 ? "Untitled" : imageData3.Title
-                                        }
-                                    </h5>
-                                    <p className="card-text">
-                                        {
-                                            imageData3 ? "No description" : imageData3.Description
-                                        }
-                                    </p>
-                                </div>
-                            </motion.div>
-
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <motion.div
-                                initial="hidden"
-                                animate="visible"
-                                exit="exit"
-                                variants={imageVariants}
-                                className="image-container card m-5">
-                                <Image className="card-img" src={
-                                    imageData4 ?
-                                        no_image :
-                                        imageData4.ImageData } fluid/>
-                                <div className="card-body">
-                                    <h5 className="card-title fw-bold">
-                                        {
-                                            imageData4 ? "Untitled" : imageData4.Title
-                                        }
-                                    </h5>
-                                    <p className="card-text">
-                                        {
-                                            imageData4 ? "No description" : imageData4.Description
-                                        }
-                                    </p>
-                                </div>
-                            </motion.div>
-                        </Col>
-                        <Col>
-                            <motion.div
-                                initial="hidden"
-                                animate="visible"
-                                exit="exit"
-                                variants={imageVariants}
-                                className="image-container card m-5">
-                                <Image className="card-img" src={
-                                    imageData5 ?
-                                        no_image :
-                                        imageData5.ImageData } fluid/>
-                                <div className="card-body">
-                                    <h5 className="card-title fw-bold">
-                                        {
-                                            imageData5 ? "Untitled" : imageData5.Title
-                                        }
-                                    </h5>
-                                    <p className="card-text">
-                                        {
-                                            imageData5 ? "No description" : imageData5.Description
-                                        }
-                                    </p>
-                                </div>
-                            </motion.div>
-                        </Col>
-                        <Col>
-                            <motion.div
-                                initial="hidden"
-                                animate="visible"
-                                exit="exit"
-                                variants={imageVariants}
-                                className="image-container card m-5">
-                                <Image className="card-img" src={
-                                    imageData6 ?
-                                        no_image :
-                                        imageData6.ImageData } fluid/>
-                                <div className="card-body">
-                                    <h5 className="card-title fw-bold">
-                                        {
-                                            imageData6 ? "Untitled" : imageData6.Title
-                                        }
-                                    </h5>
-                                    <p className="card-text">
-                                        {
-                                            imageData6 ? "No description" : imageData6.Description
-                                        }
-                                    </p>
-                                </div>
-                            </motion.div>
+                            <h3 className="text-center ">$300 - $400</h3>
+                            <ImagePortfolio image={imageData5.ImageData} title={imageData5.Title} description={imageData5.Description}/>
+                            <ImagePortfolio image={imageData6.ImageData} title={imageData6.Title} description={imageData6.Description}/>
                         </Col>
                     </Row>
                 </Container>
-                <p className='text-center'>
-                   * All prices are in Canadian Dollars.
-                </p>
-            </motion.div>
+            </div>
         </AnimatePresence>
     );
 }
